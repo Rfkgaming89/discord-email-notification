17026ecf9ec44827c3509c6f653293758a08dd4f
Kai <70678154+Rfkgaming89@users.noreply.github.com>
2024-07-27 17:04:49 -0400

    Update DiscordEmail.py

--- DiscordEmail.py
+++ DiscordEmail.py
@@ -4,10 +4,6 @@ import imaplib
 import email
 import os
 from discord.ext import commands
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Email settings
 IMAP_SERVER = os.getenv('IMAP_SERVER')
@@ -34,8 +30,8 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Define your guild and channel IDs
 GUILD_ID = int(os.getenv('GUILD_ID'))
-EMAIL_CHANNEL_ID = int(os.getenv('EMAIL_CHANNEL_ID'))
-COUNTDOWN_CHANNEL_ID = int(os.getenv('COUNTDOWN_CHANNEL_ID'))
+EMAIL_CHANNEL_ID = 1265040965492801697
+COUNTDOWN_CHANNEL_ID = 1266046599629508689
 
 # Adjusted intervals
 EMAIL_CHECK_INTERVAL = 1800  # Email check interval in seconds (30 minutes)
